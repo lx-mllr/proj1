211322d9680ab15457e60d3f10070dc4dda9abb0
Alex Miller <lxmllr71@gmail.com>
2019-01-10 19:40:52 -0800

    update gitignore, add random enemyspwan settings

---
+++ Assets/Scripts/EnemySpawn/EnemySpawnInstaller.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Zenject;
+
+public class EnemySpawnInstaller : MonoInstaller
+{
+    public List<EnemySpawnDescriptor> spawnDescriptors;
+
+    public override void InstallBindings()
+    {
+        SignalBusInstaller.Install(Container);
+
+        Container.DeclareSignal<RebindEnemySpawnSettings>().OptionalSubscriber();
+
+        Container.BindSignal<RebindEnemySpawnSettings>().ToMethod(RebindSettings);
+        RebindSettings();
+    }
+
+    private void RebindSettings() {
+        Container.Unbind<EnemySpawnSettings>();
+
+        int i, sum = 0;
+        for (i = 0; i < spawnDescriptors.Count; i++) {
+            sum += spawnDescriptors[i].percent;
+        }
+        int target = (int)(sum * UnityEngine.Random.value);
+        Debug.Log(target);
+        sum = 0;
+        for (i = 0; i < spawnDescriptors.Count; i++) {
+            sum += spawnDescriptors[i].percent;
+            if (target < sum) {
+                break;
+            }
+        }
+
+        Container.BindInstance(spawnDescriptors[i].settings).AsTransient();
+    }
+
+    [Serializable]
+    public struct EnemySpawnDescriptor {
+        public EnemySpawnSettings settings;
+        public int percent;
+    }
+}
+
+public struct RebindEnemySpawnSettings {
+}
---
+++ Assets/Scripts/ScriptableObjects/EnemySpawnSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySpawner", menuName = "SpawnerSettings/EnemySpawner")]
+public class EnemySpawnSettings : ScriptableObject
+{
+    public float SpawnRate;
+    public float MoveSpeed;
+    public float AttackStr;
+}
