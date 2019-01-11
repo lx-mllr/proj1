08c91027f20762d120e71acac5175d5edcb54e6f
Alex Miller <lxmllr71@gmail.com>
2019-01-11 14:48:00 -0800

    Add marching cube and animation to control

---
+++ Assets/Marching.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marching : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    public float moveSpeed = 1f;
+    public Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += dir * (moveSpeed * Time.deltaTime);
+    }
+}
