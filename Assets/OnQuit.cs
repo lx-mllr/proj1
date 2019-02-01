06a49bb4eb818f37f8b5856723c14d57d488228b
Alex Miller <lxmllr71@gmail.com>
2019-01-31 23:05:10 -0800

    add OnQuit mono for forcing save state

---
+++ Assets/OnQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class OnQuit : MonoBehaviour
+{
+    [Inject] readonly User _user;
+
+    void OnApplicationQuit () {
+        _user.SaveState();
+    }
+}
